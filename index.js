b1644ceb8e8dbbdee5b54d053a52469b333f2771
Noah <crashlive112@gmail.com>
2021-12-01 03:44:51 -0500

    after fetch create patient cards with their information

--- index.js
+++ index.js
@@ -11,15 +11,31 @@ const patientList = document.querySelector(".patient-list")
 function fetchPatients() {
   fetch(patientsUrl) //fetch database for information
     .then(response => response.json()) //convert retrieved response object into json object
-    .then(json => createPatientProfile(json)) //take json object and pass into function
+    .then(json => createPatientCard(json)) //take json object and pass into function
   // .catch(error => console.log(error))
 }
 
 // Handle Events
-function createPatientProfile(json) {
-  json.forEach((element) => {
+function createPatientCard(json) {
+  json.forEach((patient) => {
+    let div = document.createElement("div.patient-card")
+    let h3 = document.createElement("h3")
+    h3.innerText = `${patient.firstName} ${patient.lastName}`
+    patientList.appendChild(div)
+    div.appendChild(h3)
+    // debugger
+    addPatientInfo(patient, h3)
+  })
+}
+
+function addPatientInfo(patient, h3) {
+  Object.keys(patient).forEach((data) => {
+    console.log(data)
+    let ul = document.createElement("ul")
     let li = document.createElement("li")
-    li.innerText = `${element.firstName} ${element.lastName}`
-    patientList.appendChild(li)
+    // debugger
+    li.innerText = `${data}: ${patient[data]}`
+    ul.appendChild(li)
+    h3.appendChild(ul)
   })
 }
