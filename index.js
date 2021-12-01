16b959a837e59d58460eaba60ab69a1fa2d785c6
Noah <crashlive112@gmail.com>
2021-12-01 14:38:35 -0500

    add form functionality to submit patient info

--- index.js
+++ index.js
@@ -18,16 +18,32 @@ function fetchPatients() {
   // .catch(error => console.log(error))
 }
 
+function postPatientProfile(patientObj) {
+  fetch(patientsUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(patientObj)
+  })
+    .then(response => response.json())
+    .then(json => renderOnePatient(json))
+}
+
 // Handle Events
 function createPatientCard(json) {
   json.forEach((patient) => {
-    let div = document.createElement("div.patient-card")
-    let h4 = document.createElement("h4")
-    h4.innerText = `${patient.firstName} ${patient.lastName}`
-    patientList.appendChild(div)
-    div.appendChild(h4)
-    addPatientInfo(patient, h4)
+    renderOnePatient(patient)
   })
+}
+
+function renderOnePatient(patient) {
+  let div = document.createElement("div.patient-card")
+  let h4 = document.createElement("h4")
+  h4.innerText = `${patient.firstName} ${patient.lastName}`
+  patientList.appendChild(div)
+  div.appendChild(h4)
+  addPatientInfo(patient, h4)
 }
 
 function addPatientInfo(patient, h4) {
@@ -44,5 +60,13 @@ function addPatientInfo(patient, h4) {
 function init() {
   inputForm.addEventListener('submit', (event) => {
     event.preventDefault() //prevents form default behavior (we don't want to refresh on submit)
+    console.log(event.target.children)
+    let patientObj = {
+      firstName: event.target.children[2].value,
+      lastName: event.target.children[6].value,
+      dateOfBirth: event.target.children[10].value,
+      address: event.target.children[14].value
+    }
+    postPatientProfile(patientObj)
   })
 }
