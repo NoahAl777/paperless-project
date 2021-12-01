25e8f23827959caabfef57e9a3d35ddbe9228ca1
Noah <crashlive112@gmail.com>
2021-12-01 11:59:24 -0500

    add preventDefault to forms to avoid refreshing on submit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // On load
 document.addEventListener('DOMContentLoaded', fetchPatients)
+document.addEventListener('DOMContentLoaded', init)
 
 // Urls
 const patientsUrl = "http://localhost:3000/patients"
 const commentsUrl = "http://localhost:3000/comments"
+
 // Query Selectors
 const patientList = document.querySelector(".patient-list")
+const inputForm = document.querySelector("form")
 
 // Fetch Functions
 function fetchPatients() {
@@ -19,23 +22,27 @@ function fetchPatients() {
 function createPatientCard(json) {
   json.forEach((patient) => {
     let div = document.createElement("div.patient-card")
-    let h3 = document.createElement("h3")
-    h3.innerText = `${patient.firstName} ${patient.lastName}`
+    let h4 = document.createElement("h4")
+    h4.innerText = `${patient.firstName} ${patient.lastName}`
     patientList.appendChild(div)
-    div.appendChild(h3)
-    // debugger
-    addPatientInfo(patient, h3)
+    div.appendChild(h4)
+    addPatientInfo(patient, h4)
   })
 }
 
-function addPatientInfo(patient, h3) {
+function addPatientInfo(patient, h4) {
   Object.keys(patient).forEach((data) => {
-    console.log(data)
     let ul = document.createElement("ul")
     let li = document.createElement("li")
-    // debugger
     li.innerText = `${data}: ${patient[data]}`
     ul.appendChild(li)
-    h3.appendChild(ul)
+    h4.appendChild(ul)
+  })
+}
+
+// Form
+function init() {
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault() //prevents form default behavior (we don't want to refresh on submit)
   })
 }
