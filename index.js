49cd26969f3747fd042dee894a74137403a78208
Noah <crashlive112@gmail.com>
2021-12-01 03:04:59 -0500

    add list element for every patient name fetched

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ document.addEventListener('DOMContentLoaded', fetchPatients)
 
 // Urls
 const patientsUrl = "http://localhost:3000/patients"
+const commentsUrl = "http://localhost:3000/comments"
+// Query Selectors
+const patientList = document.querySelector(".patient-list")
 
 // Fetch Functions
 function fetchPatients() {
   fetch(patientsUrl) //fetch database for information
-    .then(response => response.json) //convert retrieved response object into json object
-    .then(json => createPatientProfile)
+    .then(response => response.json()) //convert retrieved response object into json object
+    .then(json => createPatientProfile(json)) //take json object and pass into function
+  // .catch(error => console.log(error))
 }
 
 // Handle Events
-function createPatientProfile() {
-
+function createPatientProfile(json) {
+  json.forEach((element) => {
+    let li = document.createElement("li")
+    li.innerText = `${element.firstName} ${element.lastName}`
+    patientList.appendChild(li)
+  })
 }
