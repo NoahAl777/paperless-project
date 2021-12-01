8cb9ed15cb7505b1ae96ce4c909a37125049bafb
Noah <crashlive112@gmail.com>
2021-12-01 15:36:52 -0500

    reformat patientCard rendering and add id to each card matching the object id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // On load
 document.addEventListener('DOMContentLoaded', fetchPatients)
 document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', fetchComments)
 
 // Urls
 const patientsUrl = "http://localhost:3000/patients"
@@ -10,7 +11,7 @@ const commentsUrl = "http://localhost:3000/comments"
 const patientList = document.querySelector(".patient-list")
 const inputForm = document.querySelector("form")
 
-// Fetch Functions
+// Fetch Functions - Patients
 function fetchPatients() {
   fetch(patientsUrl) //fetch database for information
     .then(response => response.json()) //convert retrieved response object into json object
@@ -30,7 +31,14 @@ function postPatientProfile(patientObj) {
     .then(json => renderOnePatient(json))
 }
 
-// Handle Events
+// Fetch Functions - Comments
+function fetchComments() {
+  fetch(commentsUrl)
+    .then(response => response.json())
+    .then(json => createCommentCard(json))
+}
+
+// Handle Events - Patients
 function createPatientCard(json) {
   json.forEach((patient) => {
     renderOnePatient(patient)
@@ -38,29 +46,36 @@ function createPatientCard(json) {
 }
 
 function renderOnePatient(patient) {
-  let div = document.createElement("div.patient-card")
+  let div = document.createElement("div")
+  div.className = "patient-card"
   let h4 = document.createElement("h4")
   h4.innerText = `${patient.firstName} ${patient.lastName}`
-  patientList.appendChild(div)
+  patientList.parentElement.appendChild(div)
   div.appendChild(h4)
   addPatientInfo(patient, h4)
 }
 
 function addPatientInfo(patient, h4) {
   Object.keys(patient).forEach((data) => {
-    let ul = document.createElement("ul")
-    let li = document.createElement("li")
-    li.innerText = `${data}: ${patient[data]}`
-    ul.appendChild(li)
-    h4.appendChild(ul)
+    // debugger
+    let patientCard = h4.parentElement
+    if (data !== "id") {
+      let ul = document.createElement("ul")
+      let li = document.createElement("li")
+      li.innerText = `${data}: ${patient[data]}`
+      ul.appendChild(li)
+      // debugger
+      patientCard.append(ul)
+    }
+    else {
+      patientCard.id = patient[data]
+    }
   })
 }
 
-// Form
 function init() {
   inputForm.addEventListener('submit', (event) => {
     event.preventDefault() //prevents form default behavior (we don't want to refresh on submit)
-    console.log(event.target.children)
     let patientObj = {
       firstName: event.target.children[2].value,
       lastName: event.target.children[6].value,
@@ -69,4 +84,9 @@ function init() {
     }
     postPatientProfile(patientObj)
   })
+}
+
+// Handle Events - Comments
+function createCommentCard(json) {
+  console.log(json)
 }
